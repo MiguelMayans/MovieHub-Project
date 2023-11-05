85a5e10964a2a28e5c172032175eeed06db8b3aa
Miguel Mayans <miguelmayansv@gmail.com>
2023-11-05 22:47:23 +0100

    add: middlewere Morgan & Helmet

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from "express"
 import userRoutes from "./routes/user.routes"
+import helmet from "helmet";
+import morgan from "morgan"
 
 const app = express()
+
 app.use(express.json())
+app.use(helmet())
+app.use(morgan("tiny"))
 
 app.use("/user", userRoutes)
 
