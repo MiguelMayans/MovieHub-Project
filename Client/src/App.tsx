62691d144be80d56fe1773796d246592dbc6af9a
Miguel Mayans <miguelmayansv@gmail.com>
2023-11-08 18:30:29 +0100

    Add: css reset and modules

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
+import '../src/styles/App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import { RouterPaths } from './routes/RouterPaths.routes'
 
 function App() {
 
   return (
     <>
       <Header />
-
+      <RouterPaths />
       <Footer />
     </>
   )
