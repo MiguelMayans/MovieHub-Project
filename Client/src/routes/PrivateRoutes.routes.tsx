ba9b7db878d735c64b410d34e5405e896e98415c
Miguel Mayans <miguelmayansv@gmail.com>
2023-12-14 19:17:05 +0100

    update:ESlint format

--- Client/src/routes/PrivateRoutes.routes.tsx
+++ Client/src/routes/PrivateRoutes.routes.tsx
@@ -1,39 +1,37 @@
-import { ReactNode, useState } from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
-import { Navigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { ReactNode, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
 
 type PrivateRoutesProps = {
-    children: ReactNode
-}
-
+  children: ReactNode;
+};
 
 const PrivateRoutes = ({ children }: PrivateRoutesProps) => {
-    const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
-    const [loading, setLoading] = useState(true);
+  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        if (!isLoading) {
-            setLoading(false);
-        }
-    }, [isLoading]);
-
-    useEffect(() => {
-        const redirect = async () => {
-            if (!isAuthenticated && !loading) {
-                await loginWithRedirect();
-            }
-        };
-
-        redirect();
-    }, [isAuthenticated, loginWithRedirect, loading]);
-
-    if (loading) {
-        return <div>Loading...</div>;
+  useEffect(() => {
+    if (!isLoading) {
+      setLoading(false);
     }
+  }, [isLoading]);
 
-    return isAuthenticated ? children : <Navigate to={"/"} />;
-}
+  useEffect(() => {
+    const redirect = async () => {
+      if (!isAuthenticated && !loading) {
+        await loginWithRedirect();
+      }
+    };
 
+    redirect();
+  }, [isAuthenticated, loginWithRedirect, loading]);
 
-export default PrivateRoutes
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return isAuthenticated ? children : <Navigate to={"/"} />;
+};
+
+export default PrivateRoutes;
